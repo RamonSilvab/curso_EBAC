d9e7d2edf321e20fcfe5c21a8e80c2eaa9c98f08
Ramon Silva Barros <ramon@dcfdci.fdci.br>
2024-07-03 13:53:18 -0300

    feat: adicionando o arquivo oo.ts

---
+++ front-end/typescript/src/oo.ts
@@ -0,0 +1,43 @@
+class Pessoa {
+    nome:string;
+    renda?: number;
+
+    constructor(nome: string, renda?: number) {
+        this.nome = nome;
+        this.renda = renda;
+    }
+
+    dizOla(): string {
+        return `${this.nome} disse Oi.`;
+    }
+}
+
+class ContaBancaria {
+    protected saldo: number = 0;
+    public numeroConta: number;
+
+    constructor(numerodaConta: number) {
+        this.numeroConta = numerodaConta;
+    }
+
+    static retornaNumeroDoBanco() {
+        return 125;
+    }
+
+    private getSaldo() {
+        return this.saldo;
+    }
+
+    depositar(valor: number) {
+        this.saldo += valor;
+    }
+}
+
+class ContaBancariaPessoaFsisica extends ContaBancaria {
+    depositar(valor: number): void {
+        this.saldo = valor * 2;
+    }
+}
+
+const contaDoPedro = new ContaBancariaPessoaFsisica(123456);
+ContaBancaria.retornaNumeroDoBanco();
